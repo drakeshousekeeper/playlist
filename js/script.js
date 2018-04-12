631ddbe514bcfd7f597ca20cd295b7204ec1e000
Syndra <syndra.j.kyddhardy@student.uncommonhs.org>
2018-04-12 00:54:25 +0000

    I FIGURED OUT THE JS!

--- js/script.js
+++ js/script.js
@@ -16,31 +16,38 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 // Songs
-var mySong = {
-	"title":"24K Magic",
-	"artist":"Bruno Mars",
-	"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
-}
+var mySong = {}
 
 var myPlayList = [
+
 	{
-		"title":"24K Magic",
-		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+		"title": "Runner's High",
+		"artist": "3racha",
+		"yt_url": "https://www.youtube.com/watch?v=SnP0Nqp455I",
+		"image_url": "https://i1.sndcdn.com/artworks-000219771617-rnlimi-t500x500.jpg",
+		"songLength": "4 minutes"
+	},
+
+	{
+		"title": "What's New, Scooby-Doo",
+		"artist": "Simple Plan",
+		"yt_url": "https://www.youtube.com/watch?v=8FQiAj5mdHY",
+		"image_url": "https://upload.wikimedia.org/wikipedia/en/thumb/8/88/Whats_new.png/250px-Whats_new.png",
+		"songLength": "2 minutes"
 	},
 	{
-		"title":"Sir Duke",
-		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"title": "Sir Duke",
+		"artist": "Stevie Wonder",
+		"yt_url": "https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
+		"image_url": "https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"songLength": "--- minutes"
 	},
 	{
-		"title":"Sorry",
-		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"title": "Sorry",
+		"artist": "Justin Bieber",
+		"yt_url": "https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
+		"image_url": "http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"songLength": "--- minutes"
 	}
 
 ]
@@ -48,27 +55,29 @@ var myPlayList = [
 
 
 // DOCUMENT READY FUNCTION
-$( document ).ready(function() {
-  
-
-
-
+$(document).ready(function() {
+	for (var i = 0; i < myPlayList.length; i = i + 1) {
+		$(".songs").append("<p><h3>" + myPlayList[i].title + "</h3></p>");
+		$(".songs").append("<p>" + myPlayList[i].artist + "</p>");
+		$(".songs").append("<p><img src='" + myPlayList[i].image_url + "'></p>");
+		$(".songs").append("<p><a href='" + myPlayList[i].yt_url + "'>Play Song</a></p>");
+	}
 });
 
-function displayList(){
+function displayList() {
 
 
-  
+
 }
 
-function clearList(){
-  
-  
-  
+function clearList() {
+
+
+
 }
 
-function addSong(){
- 
-  
-  
+function addSong() {
+
+
+
 }
